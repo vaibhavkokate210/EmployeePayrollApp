67946ca3008c14f645ee3880a4b5bc3f2fbf78c6
Vaibhav Kokate <vaibhav.kokate@morningstar.com>
2021-09-29 15:24:41 +0530

    [Vaibhav] added used Lombok library to auto generate getters and setters for DTO

--- main/java/com/bridgelabz/employeepayrollapp/model/ResponseDTO.java
+++ main/java/com/bridgelabz/employeepayrollapp/model/ResponseDTO.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.employeepayrollapp.model;
 
-public class ResponseDTO
+import lombok.Data;
+
+public @Data class ResponseDTO
 {
 	private String message;
 	private Object data;
@@ -8,19 +10,5 @@ public class ResponseDTO
 		super();
 		this.message = message;
 		this.data = data;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public Object getData() {
-		return data;
-	}
-	public void setData(Object data) {
-		this.data = data;
-	}
-	
-	
+	}	
 }
